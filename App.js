e622c965c1acc9f75e6b8e2915785beb1b7ec127
procogif <73234144+procogif@users.noreply.github.com>
2020-10-22 23:54:31 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+  }
+}
+
+export default App;
